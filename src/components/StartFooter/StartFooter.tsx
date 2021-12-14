cbc141b24dfa4377ae04b6b821e88b38b3d85a46
Alberto Del Águila <nautadev@gmail.com>
2021-12-14 19:24:30 +0100

    Added extra data in footer and also added more GA events

--- src/components/StartFooter/StartFooter.tsx
+++ src/components/StartFooter/StartFooter.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import styled from "styled-components";
 import { Container, Spacer, useTheme } from "react-neu";
 import { NavLink } from "react-router-dom";
@@ -7,33 +7,58 @@ import Split from "components/Split";
 import footers from "constants/Footers.json";
 import ReactGA from "react-ga4";
 import DarkModeSwitch from "../DarkModeSwitch";
+import ReactTooltip from "react-tooltip";
 
 const StartFooter: React.FC = () => {
-  const footerItems = footers.filter((footer) => footer.homeDisplay === true);
+  const footerItems = footers;
+  const refArray = useRef([]) as React.MutableRefObject<HTMLInputElement[]>;
 
   const { darkMode } = useTheme();
   return (
     <StyledFooter darkMode={darkMode}>
+      <ReactTooltip />
       <Spacer size="lg" />
       <Container>
         <Split>
           {footerItems.map((footer: any, index: any) => (
             <StyledDivContainer key={index}>
-              {footer.router ? (
-                <StyledRouterLink exact to={footer.url}>
+              {footer.router || !footer.homeDisplay ? (
+                <StyledRouterLink exact to={!footer.homeDisplay ? '' : footer.url} onClick={()=>{
+                  if(!footer.homeDisplay)
+                  {
+                    ReactGA.event({
+                      category: "conversion",
+                      action: `not_yet_${footer.name.toLowerCase()}`,
+                      label: "footer_bar"
+                    });
+                    for(let i=0;i<footerItems.length;i++)
+                    {
+                      ReactTooltip.hide(refArray.current[i]);
+                    }
+                    ReactTooltip.show(refArray.current[index]);
+                  }else{
+                    ReactGA.send({
+                      hitType: "pageview",
+                      page: footer.url
+                    });
+                  }
+                }}>
                   {footer.name}
                 </StyledRouterLink>
               ) : (
                 <StyledLink onClick={()=>{
                   ReactGA.event({
                     category: "conversion",
-                    action: `open_${footer.name}`,
+                    action: `open_${footer.name.toLowerCase()}`,
                     label: "footer_bar"
                   });
                 }} href={footer.url} target="_blank">
                   {footer.name}
                 </StyledLink>
               )}
+              <p ref={ref => {
+                    refArray.current[index] = ref as HTMLInputElement;
+                  }} data-effect="float" data-offset="{'left':50, 'top':10}" data-place="top" data-tip='We are working on it, check our Discord for news!'></p>
             </StyledDivContainer>
           ))}
         </Split>
